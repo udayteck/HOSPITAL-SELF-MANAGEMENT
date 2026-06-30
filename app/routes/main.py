5eba5ab46ef9ca4df56635000492605153478dd3
udayteck <uk941438@gmail.com>
2026-06-30 18:53:01 +0530

    Add home page with content

--- app/routes/main.py
+++ app/routes/main.py
@@ -5,4 +5,4 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     # Later you can render a custom home page, but for now base.html is enough
-    return render_template('base.html')
+    return render_template('index.html')
